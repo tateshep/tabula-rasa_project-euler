c933452f45359e41d3d6e1ebd341554b5a179174
tateshep <tateshep@gmail.com>
2018-07-26 12:35:10 -0700

    This is a solution
    this is a solution to the question

--- problems/p001.py
+++ problems/p001.py
@@ -9,10 +9,19 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 def find_multiples_sum(a):
     
     # list all natural numbers below 10 that are multiples of 3 or 5
-    mynums = list(range(0,(a+1),1))
-    print (mynums)
+    mynums = list(range(1,(a),1))
+    summultiples = 0
+    
+    for n in mynums:
+        if n % 3 == 0:
+            summultiples += n
+
+        elif n % 5 ==0:
+            summultiples += n
+
+    print (summultiples)
     
     # sum the above multiples
 
     
-find_multiples_sum(10)
+find_multiples_sum(1000)
