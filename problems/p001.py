d69e514142bb8679cc88d4807f0857ad48f6a453
tateshep <tateshep@gmail.com>
2018-07-26 09:30:47 -0700

    test live update
    test live update

--- problems/p001.py
+++ problems/p001.py
@@ -4,3 +4,15 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
+
+
+def find_multiples_sum(a):
+    
+    # list all natural numbers below 10 that are multiples of 3 or 5
+    mynums = range(0,a,1)
+    print (mynums)
+    
+    # sum the above multiples
+
+    
+find_multiples_sum(10)
