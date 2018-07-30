d47c822a9f00d801437c01adeb9876422d6522d0
tateshep <tateshep@gmail.com>
2018-07-29 21:49:02 -0700

    This is a solution
    It works, probably not the most efficient way of doing it

--- problems/p003.py
+++ problems/p003.py
@@ -2,29 +2,50 @@
 #
 #  What is the largest prime factor of the number 600851475143 ?
 
+import math
+
+factors = []
+myprimelist=[]
 
 
 def myfactors(a):
     
     #function to factor an input 'a'
+    end = math.ceil(math.sqrt(a))
     
-    factoring = True
-    factors = []
-    i = [1,2,3,4,5,6,7,8,9]
-    
-    while factoring == True:
-        for n in i:
-     
-            if a % i[n] ==0:
-                a =  a/i[n]
-                factors.append(a)
-            else:
-                factoring = False
-            print (factors)
-
+    for i in range(1, end):
+        if a % i == 0:
+            factors.append(i)
+            
+    return factors
 def checkprime(a):
-    
-    # function for finding if input is prime
-    pass
+    x = False
 
-myfactors(100)
+    # function for finding if input is prime
+    # Returns x =  False, if 'a' has a factor and therefore is not prime
+    # x = True , if prime  because looped through without finding factor
+    
+    
+    for i in range(2,(a),1):
+                
+        if a % i == 0:
+            x = False
+            return x 
+        
+        x = True 
+    return x 
+            
+def myprimes():
+    
+    # This function grabs primes from list of factors
+    
+    for n in range(0,len(factors)):
+                
+        if checkprime(factors[n]) == True :
+            
+            myprimelist.append(factors[n])
+
+
+myfactors(600851475143)
+myprimes()
+print(max(myprimelist))
